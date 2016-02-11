33a1b23f0250610451fb65ee51a7cc436c94c92d
Ygin White <yginwhite@gmail.com>
2016-02-11 20:46:59 +0200

    add text for X axis & make optinal column width and width between columns

--- funcs.js
+++ funcs.js
@@ -2,10 +2,12 @@
 makeChart - builds chart on user data
 params:
     opt.data(array) - user data;
-    opt.dx - offset X-asis relative canvas element;
-    opt.dy - offset Y-asis relative canvas element;
-    opt.xst - step on X - asis;
-    opt.yst - step on Y -asis;
+    opt.dx - offset X-axis relative canvas element;
+    opt.dy - offset Y-axis relative canvas element;
+    opt.xst - step on X - axis;
+    opt.yst - step on Y -axis;
+    opt.w - column width of drawing data;
+    opt.wb - width between columns;
 */
 var makeChart = function (opt) {
     //create canvas element
@@ -25,7 +27,7 @@ var makeChart = function (opt) {
     ctx.fillStyle = grad;
     ctx.fillRect(0, 0, W, H);
 
-    //draw X, Y asises
+    //draw X, Y axises
     ctx.strokeStyle = "white";
     ctx.lineWidth = "3";
     ctx.beginPath();
@@ -35,29 +37,37 @@ var makeChart = function (opt) {
     ctx.lineTo(W - opt.dx, H - opt.dy);
     ctx.stroke();
 
-    //darw text for Y asis
+    //darw text for Y axis
     ctx.fillStyle = "white";
     ctx.font = 'italic 10pt Arial';
-    for (var i = 0; i < 12; i++) {
+    ctx.beginPath(); // for lines front of the text
+    for (var i = 0; i < 10; i++) {
+        if (i == 0) continue;
         ctx.fillText(opt.yst * i + '', opt.dx - 35, H - i * opt.yst - opt.dy);
+        // draw lines
+        ctx.moveTo(opt.dx - 6, H - i * opt.yst - opt.dy);
+        ctx.lineTo(opt.dx + 2, H - i * opt.yst - opt.dy);
     };
+    ctx.stroke();
 
     //darw data
-    var coord = []; //for text on X asis
+    var coord = []; //for text on X axis
     ctx.fillStyle = 'blue';
     for (var i = 0; i < opt.data.length; i++) {
-        var x = i * 50 + opt.dx,
-        w = 30,
+        var x = i * (opt.w + opt.wb) + opt.dx,
         h = opt.data[i],
         y = H - h - opt.dy;
         coord.push(x);
-        ctx.fillRect(x, y, w, h);
+        ctx.fillRect(x, y, opt.w, h);
     }
 
-    //draw text for X asis
+    //draw text for X axis
     ctx.fillStyle = "white";
     for (var i = 0; i < coord.length; i++) {
-        ctx.fillText(coord[i] + '', opt.dx + coord[i] - 30, H - opt.dy + 25);
+        ctx.fillText(opt.data[i] + '', opt.dx / 2 + coord[i] - 25, H - opt.dy + 25);
+        if (opt.text !== undefined) {
+            ctx.fillText(opt.text[i] + '', opt.dx / 2 + coord[i] - 25, H - opt.dy + 25 + 20);
+        }
     };
 
     //append to ...
